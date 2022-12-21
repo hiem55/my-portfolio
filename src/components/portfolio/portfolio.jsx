f380c3c7b5694d2c56f9d9824304677cbea10699
hiem55 <100002888+hiem55@users.noreply.github.com>
2022-12-20 17:09:13 -0800

    Complete template for all sections

---
+++ src/components/portfolio/portfolio.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './portfolio.css'
+
+const portfolio = () => {
+    return (
+        <div>
+            portfolio
+        </div>
+    )
+}
+
+export default portfolio
