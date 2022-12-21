af8df3eb488688d9695d28e65bc34f729ca2d324
hiem55 <100002888+hiem55@users.noreply.github.com>
2022-12-20 17:38:53 -0800

    initialize imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import Header from './components/header/header'
+import Nav from './components/nav/nav'
+import About from './components/about/about'
+import Experience from './components/experience/experience'
+import Services from './components/services/services'
+import Portfolio from './components/portfolio/portfolio'
+import Contact from './components/contact/contact'
+import Footer from './components/footer/footer'
 
 const App = () => {
     return (
-        <div>
-            app
-        </div>
+        <>
+            <Header />
+            <Nav />
+            <About />
+            <Experience />
+            <Services />
+            <Portfolio />
+            <Contact />
+            <Footer />
+
+        </>
     )
 }
 
