9f0fb17508f51efb3e2ae58d3f388d99f789be85
hiem55 <100002888+hiem55@users.noreply.github.com>
2022-12-22 01:47:33 -0800

    Complete footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import './footer.css'
+import { BsLinkedin } from 'react-icons/bs'
 
 const footer = () => {
     return (
-        <div>
-            footer
-        </div>
+
+        <footer>
+            <a href="#" className='footer_logo'>Justin</a>
+
+            <ul className='permalinks'>
+                <li><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#hobbies">Hobbies</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+            <div className='footer_socials'>
+                <a href="https://linkedin.com"><BsLinkedin /></a>
+
+            </div>
+
+            <div className="footer_copyright">
+                <small>&copy; Justin Oh All rights reserved. 2022</small>
+            </div>
+        </footer>
     )
 }
 
