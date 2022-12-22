82c3d95a1e513b2c0b7e0cb72c3e489477aead85
hiem55 <100002888+hiem55@users.noreply.github.com>
2022-12-22 02:38:59 -0800

    build app

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -9,21 +9,24 @@ const data = [
         id: 1,
         image: IMG1,
         title: "Lost Ark Bot",
-        github: "https://github.com",
+        github: "https://github.com/hiem55/LostArkBot",
+        tech: "Python (Pyautogui, OpenCV, Numpy)",
         demo: "https://github.com"
     },
     {
         id: 2,
         image: IMG2,
         title: "co.Cost Database for information about cost of living near your school",
-        github: "https://github.com",
+        github: "https://github.com/maz1995/1800_202230_DTC05",
+        tech: "HTML, CSS, Bootstrap, JavaScript, Jquery, Firebase/Firestore",
         demo: "https://github.com"
     },
     {
         id: 3,
         image: IMG3,
         title: "League of Legends text Adventure",
-        github: "https://github.com",
+        github: "https://github.com/hiem55/comp-1510-a4-justin",
+        tech: "Python",
         demo: "https://github.com"
     },
 ]
@@ -35,12 +38,14 @@ const Projects = () => {
 
             <div className="container projects_container">
                 {
-                    data.map(({ id, image, title, github, demo }) => {
+                    data.map(({ id, image, title, github, tech, demo }) => {
                         return (
                             <article key={id} className='project_item'>
                                 <div className="project_item-image">
                                     <img src={image} alt="project" />
                                     <h3>{title}</h3>
+                                    <h4>Skills</h4>
+                                    <p>{tech}</p>
                                     <div className="project_item-cta">
                                         <a href={github} className='btn' target='_blank'>Github</a>
                                         <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
